eab98ad9a092d7cd45b57eb87e56d2c1b790bd60
Lokeswaran Aruljothy <11lokeswaran@gmail.com>
2024-10-12 08:50:58 +0530

    🚀 Initial project setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+
+import { GeistSans } from "geist/font/sans";
+
+import "./globals.css";
+
+const geistSans = GeistSans;
+
+export const metadata: Metadata = {
+    title: "Next.js Starter app",
+    description: "A basic starter template created by Lokeswaran Aruljothy",
+};
+
+const RootLayout = ({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) => {
+    return (
+        <html lang="en">
+            <body className={`${geistSans.variable} antialiased`}>{children}</body>
+        </html>
+    );
+};
+
+export default RootLayout;
